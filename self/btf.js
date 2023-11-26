a7c4a3860c4ae79d77bebbb8cf89325485407ede
Shannon Hung <hsuanhung036@gmail.com>
2023-11-26 13:25:21 +0100

    Shannon's Blog

--- self/btf.js
+++ self/btf.js
@@ -1,27 +1,27 @@
 (() => {
-    const isIncludeEN = item => {
-      const key = '/en/'
-      return item.includes(key)
-    }
-  
-    window.loadFullPage = (url) => {
-      window.location.href = url
-    }
-  
-    const eventFn = (elements, includeEN) => {
-      elements.forEach(item => {
-        console.log(`!includeEN || !isIncludeEN(item.href) = ${!includeEN || !isIncludeEN(item.href)}`)
-        if (!includeEN || !isIncludeEN(item.href)) {
-          console.log(item.href)
-          item.href = `javascript:loadFullPage('${item.href}');`
-        }
-      })
-    }
-  
-    const nowIncludeEN = isIncludeEN(window.location.href)
-    const selector = nowIncludeEN
-      ? document.querySelectorAll('a[href^="https://ShannonHung.github.io"]')
-      : document.querySelectorAll('a[href^="/en/"]')
-  
-    eventFn(selector, nowIncludeEN)
-  })()
+  const isIncludeEN = item => {
+    const key = '/en/'
+    return item.includes(key)
+  }
+
+  window.loadFullPage = (url) => {
+    window.location.href = url
+  }
+
+  const eventFn = (elements, includeEN) => {
+    elements.forEach(item => {
+      console.log(`!includeEN || !isIncludeEN(item.href) = ${!includeEN || !isIncludeEN(item.href)}`)
+      if (!includeEN || !isIncludeEN(item.href)) {
+        console.log(item.href)
+        item.href = `javascript:loadFullPage('${item.href}');`
+      }
+    })
+  }
+
+  const nowIncludeEN = isIncludeEN(window.location.href)
+  const selector = nowIncludeEN
+    ? document.querySelectorAll('a[href^="https://ShannonHung.github.io"]')
+    : document.querySelectorAll('a[href^="/en/"]')
+
+  eventFn(selector, nowIncludeEN)
+})()
