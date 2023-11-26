c450640633ebbc33ddd0821c0dc4be601c6db10a
Shannon Hung <hsuanhung036@gmail.com>
2023-11-26 03:22:40 +0100

    Shannon's Blog

---
+++ self/btf.js
@@ -0,0 +1,25 @@
+(() => {
+    const isIncludeEN = item => {
+      const key = '/en/'
+      return item.includes(key)
+    }
+  
+    window.loadFullPage = (url) => {
+      window.location.href = url
+    }
+  
+    const eventFn = (elements, includeEN) => {
+      elements.forEach(item => {
+        if (!includeEN || !isIncludeEN(item.href)) {
+          item.href = `javascript:loadFullPage('${item.href}');`
+        }
+      })
+    }
+  
+    const nowIncludeEN = isIncludeEN(window.location.href)
+    const selector = nowIncludeEN
+      ? document.querySelectorAll('a[href^="https://localhost:4000"]')
+      : document.querySelectorAll('a[href^="/en/"]')
+  
+    eventFn(selector, nowIncludeEN)
+  })()
